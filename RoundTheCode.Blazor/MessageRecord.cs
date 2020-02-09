f96b363b5e80d88b6f0718e4fa0b1a01c36dc567
Dave <davidgrace00@gmail.com>
2020-02-09 20:16:58 +0000

    Added Messages Component to Blazor Component

---
+++ RoundTheCode.Blazor/MessageRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RoundTheCode.Blazor
+{
+    public class MessageRecord
+    {
+        public virtual string Message { get; set; }
+
+        public virtual DateTimeOffset Sent { get; }
+
+        public virtual bool Verified { get; set; }
+
+        public MessageRecord(string message, DateTimeOffset sent)
+        {
+            Message = message;
+            Sent = sent;
+        }
+    }
+}
